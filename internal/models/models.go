1fd4e2df517ce035887ccad587667dce81e6c1db
leftyy <ustyantsev.makar1@gmail.com>
2025-05-31 09:47:23 +0300

    chore: go fmt

--- internal/models/models.go
+++ internal/models/models.go
@@ -3,12 +3,12 @@ package models
 type Quote struct {
 	ID     int    `json:"id"`
 	Author string `json:"author"`
-	Quote   string `json:"quote"`
+	Quote  string `json:"quote"`
 }
 
 type CreateQuoteDTO struct {
 	Author string `json:"author"`
-	Quote   string `json:"quote"`
+	Quote  string `json:"quote"`
 }
 
 func (c CreateQuoteDTO) Valid() bool {
